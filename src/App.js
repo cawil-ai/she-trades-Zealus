ffc64ffda2a1a2583651f4be5f42f60078b3284e
amberitas17 <67531380+amberitas17@users.noreply.github.com>
2022-02-27 19:25:18 +0800

    frontend updates

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./icons.js";
+import Index from "./screens/Index";
+
+function App() {
+  return (
+    <Router>
+      <Route path="/" exact component={Index} />
+      <Route path="/Index/" exact component={Index} />
+    </Router>
+  );
+}
+
+export default App;
